370ba5f34aa82aaf417ac312af6b37e3749ef0a9
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-05-10 22:33:40 -0600

    Changed to squares because of issues

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -53,11 +53,28 @@ public class Snake : MonoBehaviour
         _segments.Add(segment);
     }
 
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        this.transform.position = Vector3.zero;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Food")
         {
             Grow();
+        }
+        else if (collision.tag == "Obstacle")
+        {
+            ResetState();
         }
     }
 
