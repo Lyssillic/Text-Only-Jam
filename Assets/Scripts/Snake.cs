407e2647df1c83d7b28da319cc7e8793fed3b079
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-05-13 22:05:39 -0600

    Added head/corner logic from Zigurous

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,9 +4,21 @@ using System.Collections.Generic;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
-    private List<Transform> _segments = new List<Transform>();
-    public Transform segmentPrefab;
+    private List<SnakeSegment> _segments = new List<SnakeSegment>();
+    private SnakeSegment _head;
     public int initialSize = 4;
+    public SnakeSegment segmentPrefab;
+
+    private void Awake()
+    {
+        _head = GetComponent<SnakeSegment>();
+
+        if (_head == null)
+        {
+            _head = this.gameObject.AddComponent<SnakeSegment>();
+            _head.hideFlags = HideFlags.HideInInspector;
+        }
+    }
 
     private void Start()
     {
@@ -15,60 +27,65 @@ public class Snake : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (_head.direction.x != 0.0f)
         {
-            _direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S))
-        {
-            _direction = Vector2.down;
-        } else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
-        } else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _head.SetDirection(Vector2.up, Vector2.zero);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _head.SetDirection(Vector2.down, Vector2.zero);
+            }
         }
-
+        else if (_head.direction.y != 0.0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                _head.SetDirection(Vector2.right, Vector2.zero);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                _head.SetDirection(Vector2.left, Vector2.zero);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         for (int i = _segments.Count - 1; i > 0; i--)
         {
-            _segments[i].position = _segments[i - 1].position;
+            _segments[i].Follow(_segments[i - 1], i, _segments.Count);
         }
-
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x + _direction.x),
-            Mathf.Round(this.transform.position.y + _direction.y),
-            0.0f
-        );
+        _head.transform.position = new Vector3(
+            Mathf.Round(_head.transform.position.x) + _head.direction.x,
+            Mathf.Round(_head.transform.position.y) + _head.direction.y);
     }
 
-    private void Grow()
+    public void Grow()
     {
-        Transform segment = Instantiate(this.segmentPrefab);
-        segment.position = _segments[_segments.Count - 1].position;
-
+        SnakeSegment segment = Instantiate(this.segmentPrefab);
+        segment.Follow(_segments[_segments.Count - 1], _segments.Count, _segments.Count + 1);
         _segments.Add(segment);
     }
 
-    private void ResetState()
+    public void ResetState()
     {
+        _head.SetDirection(Vector2.right, Vector2.zero);
+        _head.transform.position = Vector3.zero;
+
         for (int i = 1; i < _segments.Count; i++)
         {
             Destroy(_segments[i].gameObject);
         }
 
         _segments.Clear();
-        _segments.Add(this.transform);
+        _segments.Add(_head);
 
-        for (int i = 0; i < this.initialSize; i++)
+        for (int i = 0; i < this.initialSize - 1; i++)
         {
-            _segments.Add(Instantiate(this.segmentPrefab));
+            Grow();
         }
-
-        this.transform.position = Vector3.zero;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -82,5 +99,4 @@ public class Snake : MonoBehaviour
             ResetState();
         }
     }
-
 }
